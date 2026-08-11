4281c04d5e7043e3173c10c144587307d2f7954d
Liam Reimers <liam@shurloc.com>
2026-08-11 14:22:23 -0700

    Update PHPStan bootstrap with constants for type discovery

--- tests/phpstan-bootstrap.php
+++ tests/phpstan-bootstrap.php
@@ -4,3 +4,30 @@
  *
  * @package ShurlocCustomerTools
  */
+
+declare( strict_types=1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/../' );
+}
+
+if ( ! defined( 'SHURLOC_CUSTOMER_TOOLS_VERSION' ) ) {
+	define(
+		'SHURLOC_CUSTOMER_TOOLS_VERSION',
+		'0.1.0'
+	);
+}
+
+if ( ! defined( 'SHURLOC_CUSTOMER_TOOLS_PATH' ) ) {
+	define(
+		'SHURLOC_CUSTOMER_TOOLS_PATH',
+		__DIR__ . '/'
+	);
+}
+
+if ( ! defined( 'SHURLOC_CUSTOMER_TOOLS_URL' ) ) {
+	define(
+		'SHURLOC_CUSTOMER_TOOLS_URL',
+		'https://example.com/wp-content/plugins/shurloc-customer-tools/'
+	);
+}
