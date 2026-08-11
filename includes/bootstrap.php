606f33689365d4a0c4fa822c60707d1f2297e13b
Liam Reimers <liam@shurloc.com>
2026-08-11 14:18:27 -0700

    Add autoloader

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -14,7 +14,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Bootstrap the plugin.
  */
-function shurloc_product_tools_bootstrap(): void {
+function shurloc_customer_tools_bootstrap(): void {
+	/*
+	 * Autoloader.
+	 */
+
+	require_once SHURLOC_CUSTOMER_TOOLS_PATH . 'includes/class-shurloc-autoloader.php';
+
+	$autoloader = new Shurloc_Autoloader(
+		__DIR__
+	);
+
+	$autoloader->register();
 }
 
 add_action(
