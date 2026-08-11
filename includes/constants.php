ba138c21122267bb223d363cbf27fa9316a3df3e
Liam Reimers <liam@shurloc.com>
2026-08-11 14:15:09 -0700

    Add basic constants

--- includes/constants.php
+++ includes/constants.php
@@ -4,3 +4,24 @@
  *
  * @package ShurlocCustomerTools
  */
+
+declare( strict_types=1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Plugin version.
+ */
+define( 'SHURLOC_CUSTOMER_TOOLS_VERSION', '0.1.0' );
+
+/**
+ * Plugin directory path.
+ */
+define( 'SHURLOC_CUSTOMER_TOOLS_PATH', plugin_dir_path( __DIR__ ) );
+
+/**
+ * Plugin directory URL.
+ */
+define( 'SHURLOC_CUSTOMER_TOOLS_URL', plugin_dir_url( __DIR__ ) );
