4fdbf14d88287a14f8f2995a5a2ca578a0a0985a
Liam Reimers <liam@shurloc.com>
2026-08-11 14:17:23 -0700

    Add bootstrap skeleton

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -4,3 +4,20 @@
  *
  * @package ShurlocCustomerTools
  */
+
+declare( strict_types=1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Bootstrap the plugin.
+ */
+function shurloc_product_tools_bootstrap(): void {
+}
+
+add_action(
+	'plugins_loaded',
+	'shurloc_customer_tools_bootstrap'
+);
